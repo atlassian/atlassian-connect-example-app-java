e190e578f6eb874297110c9076907b2da333a0e6
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-02-28 10:07:12 +1100

    - Static files added

--- src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/AtlassianConnectSampleAppJavaApplication.java
+++ src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/AtlassianConnectSampleAppJavaApplication.java
@@ -1,9 +1,17 @@
 package com.sample.connect.app.atlassianconnectsampleappjava;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
-public class AtlassianConnectSampleAppJavaApplication {
+public class AtlassianConnectSampleAppJavaApplication implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtlassianConnectSampleAppJavaApplication.class, args);
