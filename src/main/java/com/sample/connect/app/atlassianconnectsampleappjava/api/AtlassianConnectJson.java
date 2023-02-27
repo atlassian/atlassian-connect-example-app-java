da8500415d7fa92e9fde738a20983445e0ab2b97
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-02-28 10:50:40 +1100

    - Updating connect json

--- src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/api/AtlassianConnectJson.java
+++ src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/api/AtlassianConnectJson.java
@@ -2,8 +2,10 @@ package com.sample.connect.app.atlassianconnectsampleappjava.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,5 +17,16 @@ public class AtlassianConnectJson {
     public JsonNode atlassianConnect() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(new File("atlassian-connect.json"), JsonNode.class);
+    }
+
+    @GetMapping("/config")
+    public ModelAndView config(ModelMap model) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readValue(new File("atlassian-connect.json"), JsonNode.class);
+
+        model.put("index", "Connect app descriptor");
+        model.put("config", jsonNode);
+
+        return new ModelAndView("config", model);
     }
 }
