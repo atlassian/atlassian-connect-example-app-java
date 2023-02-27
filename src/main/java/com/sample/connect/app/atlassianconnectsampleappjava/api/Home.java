56ad95c3f2ddf6f19f6ec8aa3eaec70b47e6963c
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-02-27 17:14:55 +1100

    - Mustache templating added

--- src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/api/Home.java
+++ src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/api/Home.java
@@ -1,14 +1,18 @@
 package com.sample.connect.app.atlassianconnectsampleappjava.api;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Home {
 
     @GetMapping("/")
-    public String home() {
-        // TODO: Add templates for view
-        return "This is the home page";
+    public ModelAndView home(ModelMap model) {
+        model.put("index", "Home Page");
+        model.put("body", "You in the home page!");
+
+        return new ModelAndView("home", model);
     }
 }
