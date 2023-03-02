d71de381ac2887b5c730aba5033a4cd469a3e571
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-03-02 16:31:51 +1100

    - Minor revert

--- src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/api/Home.java
+++ src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/api/Home.java
@@ -1,7 +1,5 @@
 package com.sample.connect.app.atlassianconnectsampleappjava.api;
 
-import com.sample.connect.app.atlassianconnectsampleappjava.repository.TenantRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Home {
-    @Autowired
-    TenantRepository tenantRepository;
-
     @GetMapping("/")
     public ModelAndView home(ModelMap model) {
         model.put("index", "Home Page");
