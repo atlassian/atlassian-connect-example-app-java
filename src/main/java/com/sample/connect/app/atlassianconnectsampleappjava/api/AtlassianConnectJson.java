fb3e17d47c6add2516def09fa3ef29bbede2ce2b
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-03-03 16:48:16 +1100

    - Prettify string in config view

--- src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/api/AtlassianConnectJson.java
+++ src/main/java/com/sample/connect/app/atlassianconnectsampleappjava/api/AtlassianConnectJson.java
@@ -2,6 +2,7 @@ package com.sample.connect.app.atlassianconnectsampleappjava.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,10 +22,13 @@ public class AtlassianConnectJson {
     @GetMapping("/config")
     public ModelAndView config(ModelMap model) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
         JsonNode jsonNode = mapper.readValue(new File("atlassian-connect.json"), JsonNode.class);
+        String jsonString = mapper.writeValueAsString(jsonNode);
 
         model.put("index", "Connect app descriptor");
-        model.put("config", jsonNode);
+        model.put("config", jsonString);
 
         return new ModelAndView("config", model);
     }
